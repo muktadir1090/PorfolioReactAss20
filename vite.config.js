b9c8a118ddeac41dbd16b4bd3c2a1a32fb1c72e9
Tuba Muktadir <muktadir1090@gmail.com>
2024-10-05 18:42:46 -0400

    update json file

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
-    open: true
+    host: '0.0.0.0',  // Binds to all available network interfaces
+    open: true        // Automatically opens the app in the browser
   }
 })
