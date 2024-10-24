48233a8ab9bd8b7ff590cafd8b60943388b0c171
Tuba Muktadir <muktadir1090@gmail.com>
2024-10-24 17:11:14 -0400

    updated link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     <footer>
       <a href="https://github.com/muktadir1090" target="_blank" rel="noreferrer">GitHub</a>
       <a href="https://www.linkedin.com/in/tuba-m-4ba104124/r" target="_blank" rel="noreferrer">LinkedIn</a>
-      <a href="https://stackoverflow.com/users/developer" target="_blank" rel="noreferrer">Stack Overflow</a>
+      <a href="https://stackoverflow.com/users/21884746/sm16-dev123" target="_blank" rel="noreferrer">Stack Overflow</a>
     </footer>
   );
 }
